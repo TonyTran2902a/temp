2f645228635e7390e895e3104a0b2afbcb4d1a77
TonyTran2902a <165017012+TonyTran2902a@users.noreply.github.com>
2024-12-27 00:47:25 +0700

    Add files via upload

---
+++ buttonController.js
@@ -0,0 +1,277 @@
+const controller = {};
+const models = require("../models");
+
+  controller.showCommentOverlay = async (req, res) => {
+    const postid = req.params.postid;
+    const userId = req.session.userId;
+
+    console.log("User ID:", userId);
+    res.locals.currentUser = await models.User.findByPk(userId, (err, user) => {
+      if (err) {
+        return res.status(500).send('Error retrieving user information');
+      }
+    });
+
+    const thread = await models.Thread.findOne({
+      attributes: ['id', 'user_id', 'content', 'createdAt'],
+      include: [
+        {model: models.User},
+        {model: models.Comment,
+          include: [{model: models.User,
+            attributes: ['username', 'profile_picture']
+          }]
+        },
+        {model: models.Media},
+      ],
+      where: {
+        id: postid
+      }
+    });
+
+    console.log(thread);
+    res.locals.thread = thread;
+    res.render("comment-overlay", {layout:false});
+  }
+
+  controller.showRepostOverlay = async (req, res) => {
+    const postid = req.params.postid;
+    const userId = req.session.userId;
+  
+    console.log("User ID:", userId);
+    res.locals.currentUser = await models.User.findByPk(userId, (err, user) => {
+      if (err) {
+        return res.status(500).send('Error retrieving user information');
+      }
+    });
+  
+    const thread = await models.Thread.findOne({
+      attributes: ['id', 'user_id', 'content', 'createdAt'],
+      include: [
+        {model: models.User},
+        {model: models.Comment,
+          include: [{model: models.User,
+            attributes: ['username', 'profile_picture']
+          }]
+        },
+        {model: models.Media},
+      ],
+      where: {
+        id: postid
+      }
+    });
+  
+    console.log(thread);
+    res.locals.thread = thread;
+    res.render("repost-overlay", {layout:false});
+  }
+
+
+  controller.likePost = async (req, res) => {
+      const currentUserId = parseInt(req.session.userId);
+      const threadId = parseInt(req.params.id);
+      const targetUserId = await models.Thread.findOne({
+        where: { id: threadId },
+        attributes: ['user_id'],
+      });
+      try {
+        await models.Like.create({
+            user_id: currentUserId,
+            thread_id: threadId,
+        });
+        if (targetUserId.user_id != currentUserId) {
+          await models.Notification.create({
+            content: "Like your post !",
+            redirect_url: `/Homepage/${threadId}`,
+            transferer_id: currentUserId,
+            receiver_id: targetUserId.user_id,
+          });
+        }
+        const likeCount = await models.Like.count({ where: { thread_id: threadId } });
+        res.status(200).json({ isLiked: true, likeCount });
+
+        } catch (err) {
+        res.status(500).send('Error like the post');
+        }
+  }
+  
+  controller.unlikePost = async (req, res) => {
+    const currentUserId = parseInt(req.session.userId);
+    const threadId = parseInt(req.params.id);
+    console.log("current:", currentUserId);
+    console.log("target:", threadId);
+  
+    try {
+    const likedPost = await models.Like.findOne({
+        where: {
+            user_id: currentUserId,
+            thread_id: threadId,
+        },
+    });
+  
+  
+    await likedPost.destroy(); // Unfollow the user
+    
+    // await models.Notification.destroy({
+    //     where: {
+    //     transferer_id: currentUserId,
+    //     receiver_id: targetUserId,
+    //     },
+    // });
+    const likeCount = await models.Like.count({ where: { thread_id: threadId } });
+    res.status(200).json({ isLiked: false, likeCount });
+    } catch (err) {
+    res.status(500).send('Error unlike the post');
+    }
+  }
+  
+
+  controller.addPost = async(req, res) => {
+    const {userId, content, image, tag} = req.body;
+
+    const newPost = await models.Thread.create({
+        content,
+        user_id: userId,
+    })
+
+    // Handle the uploaded image (if provided)
+    if (req.file) {
+        const mediaUrl = `/img/post/${req.file.filename}`;
+        await models.Media.create({
+            mediaUrl,
+            thread_id: newPost.id, // Associate with the newly created post
+        });
+    }
+
+    return res.redirect(`/Homepage/${newPost.id}`);
+}
+
+  controller.addComment = async (req, res) => {
+    try {
+      const threadId = req.params.postid; // Get thread ID from URL
+      const { user_id, content, page } = req.body; // Get data from form inputs
+
+      const targetUserId = await models.Thread.findOne({
+        where: { id: threadId },
+        attributes: ['user_id'],
+      });
+      // Input validation
+      if (!user_id || !threadId || !content) {
+          return res.status(400).send('All fields are required.');
+      }
+      // Save the comment to your database
+        // Save the comment
+      await models.Comment.create({
+          user_id: user_id,
+          thread_id: threadId,
+          content: content
+      });
+      if (user_id != targetUserId.user_id) {
+        await models.Notification.create({
+          content: "Comment on your post !",
+          redirect_url: `/Homepage/${threadId}`,
+          transferer_id: user_id,
+          receiver_id: targetUserId.user_id,
+        });
+      }
+
+      res.redirect(`/Homepage/${threadId}`);
+  } catch (error) {
+      console.error('Error saving comment:', error);
+      res.status(500).send('An error occurred while saving the comment.');
+  }
+  }
+  
+  controller.addRepost = async (req, res) => {
+      try {
+        const threadId = req.params.postid; // Get thread ID from URL
+        const { user_id, thread_user_id,  page } = req.body; // Get data from form inputs
+        // Input validation
+        if (!user_id || !threadId) {
+            return res.status(400).send('All fields are required.');
+        }
+        const existingRepost = await models.Repost.findOne({
+          where: {
+              user_id: user_id,
+              thread_id: threadId,
+          },
+        });
+        if (existingRepost) {
+          // If repost already exists, send notification
+          //return res.status(400).json({ success: false, message: 'You have already reposted this thread.' });
+          return res.redirect(`/Profile/?tab=repost`);
+        }
+        else if (user_id == thread_user_id) {
+          //return res.status(400).json({ success: false, message: 'You cannot repost your own thread.' });
+          return res.redirect(`/Profile/`);
+        }
+        if (user_id != thread_user_id) {
+          await models.Notification.create({
+            content: "Repost your post !",
+            redirect_url: `/Profile/${user_id}?tab=repost`,
+            transferer_id: user_id,
+            receiver_id: thread_user_id,
+          });
+        }
+        // Save the repost to your database
+        await models.Repost.create({
+            user_id: user_id,
+            thread_id: threadId,
+        });
+        res.redirect(`/Profile/?tab=repost`);
+    } catch (error) {
+        console.error('Error saving repost:', error);
+        res.status(500).send('An error occurred while saving the repost.');
+    }
+  }
+
+  controller.addFollower = async (req, res) => {
+    const currentUserId = parseInt(req.session.userId);
+    const targetUserId = parseInt(req.params.id);
+    console.log("current:", currentUserId);
+    console.log("target:", targetUserId);
+
+    try {
+      await models.Follower.create({
+        follower_id: currentUserId,
+        following_id: targetUserId,
+      });
+
+      await models.Notification.create({
+        content: "Follow you",
+        redirect_url: `/Profile/${currentUserId}`,
+        transferer_id: currentUserId,
+        receiver_id: targetUserId,
+      });
+
+      res.status(200).send('Followed successfully');
+    } catch (err) {
+      res.status(500).send('Error following the user');
+    }
+  };
+
+// Function to unfollow a user (DELETE)
+  controller.deleteFollower = async (req, res) => {
+  const currentUserId = parseInt(req.session.userId);
+  const targetUserId = parseInt(req.params.id);
+
+  try {
+    const existingFollow = await models.Follower.findOne({
+      where: {
+        follower_id: currentUserId,
+        following_id: targetUserId,
+      },
+    });
+
+    if (!existingFollow) {
+      return res.status(400).send('You are not following the user');
+    }
+
+    await existingFollow.destroy(); // Unfollow the user
+
+    res.status(200).send('Unfollowed successfully');
+  } catch (err) {
+    res.status(500).send('Error unfollowing the user');
+  }
+  };
+  
+  module.exports = controller;
---
+++ pageController.js
@@ -0,0 +1,663 @@
+const controller = {};
+const models = require("../models");
+
+const bcrypt = require('bcrypt'); // Ensure password security
+const { Op, where } = require('sequelize');
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+const { fn, col, literal } = require("sequelize");
+
+
+controller.getUnreadNoti = async (req, res, next) => {
+  const userId = req.session.userId;
+  const notifications = await models.Notification.findAll({
+    include: [
+      {
+        model: models.User,
+        as: 'transfererFk', // Specify alias for transferer_id
+      },
+    ],
+    where: { receiver_id: userId },
+    order: [["id", "ASC"]],
+  });
+  res.locals.unreadNotiNum = notifications.filter(notification => !notification.is_read).length;
+  next();
+}
+
+controller.showHomepage = async (req, res) => {
+  const userId = req.session.userId;
+  const { view } = req.query;
+
+  res.locals.currentUser = await models.User.findByPk(userId, (err, user) => {
+    if (err) {
+      return res.status(500).send("Error retrieving user information");
+    }
+  });
+  res.locals.loggingInUser = res.locals.currentUser;
+
+  const likedThreadIds = await models.Thread.findAll({
+    attributes: ["id"], 
+    include: [
+      {
+        model: models.Like,
+        required: true, 
+        where: { user_id: userId },
+      },
+    ],
+    raw: true, 
+  }).then((threads) => threads.map((thread) => thread.id)); 
+
+  if (view === "following") {
+    res.locals.threads = (
+      await models.Thread.findAll({
+        attributes: {
+          include: [[fn("COUNT", col("Likes.id")), "likeCount"]],
+        },
+        include: [
+          {
+            model: models.User, 
+            required: true,
+            include: [
+              {
+                model: models.Follower, 
+                where: { follower_id: userId }, 
+                required: true, 
+                attributes: [], 
+              },
+            ],
+          },
+          { model: models.Media },
+          {
+            model: models.Like,
+            attributes: [],
+            required: false,
+          },
+          {
+            model: models.Comment,
+            attributes: ["id"],
+            required: false,
+          },
+          {
+            model: models.Repost,
+            attributes: ["id"],
+            required: false,
+          },
+        ],
+        group: ["Thread.id", "User.id", "Media.id", "Comments.id", "Reposts.id"],
+        order: [["id", "ASC"]],
+      })
+    ).map((thread) => {
+      const plainThread = thread.get({ plain: true });
+  
+      plainThread.isLiked = likedThreadIds.includes(plainThread.id);
+      return plainThread;
+    });
+  } else {
+    res.locals.threads = (
+      await models.Thread.findAll({
+        attributes: {
+          include: [[fn("COUNT", col("Likes.id")), "likeCount"]],
+        },
+        include: [
+          { model: models.User },
+          { model: models.Media },
+          {
+            model: models.Like,
+            attributes: [],
+            required: false,
+          },
+          {
+            model: models.Comment,
+            attributes: ["id"],
+            required: false,
+          },
+          {
+            model: models.Repost,
+            attributes: ["id"],
+            required: false,
+          },
+        ],
+        group: [
+          "Thread.id",
+          "User.id",
+          "Media.id",
+          "Comments.id",
+          "Reposts.id",
+        ],
+        order: [["createdAt", "ASC"]],
+      })
+    ).map((thread) => {
+      const plainThread = thread.get({ plain: true });
+
+      plainThread.isLiked = likedThreadIds.includes(plainThread.id);
+      return plainThread;
+    });
+
+    // res.locals.threads.forEach(thread => {
+    //   console.log(thread);
+    // })
+  }
+
+  res.render("homepage", { headerName: "Home", page: 1 });
+};
+
+controller.showSearch = async (req, res) => {
+  const userId = req.session.userId;
+  const { searching = "" } = req.query;
+
+  if (searching.trim() != "") {
+    let threadOptions = {
+      include: [
+        { model: models.User },
+        {
+          model: models.Media,
+          attributes: ['id', 'mediaUrl', 'createdAt'] // Specify which fields you want from Media
+        },
+      ],
+      where: {
+        // user_id: {
+        //   [Op.ne]: userId, 
+        // },
+      },
+      order: [["createdAt", "ASC"]],
+    };
+
+    let userOptions = {
+      where: {},
+      order: [["createdAt", "ASC"]],
+    };
+
+    threadOptions.where[Op.or] = {
+      content: { [Op.iLike]: `%${searching.trim()}%` },
+    };
+
+    userOptions.where[Op.or] = {
+      username: { [Op.iLike]: `%${searching.trim()}%` },
+      bio: { [Op.iLike]: `%${searching.trim()}%` },
+    };
+
+    let threads = await models.Thread.findAll({ ...threadOptions });
+    let users = await models.User.findAll({ ...userOptions });
+
+    threads = threads.map((thread) => ({ ...thread.toJSON(), type: "thread" }));
+    users = users.map((user) => ({ ...user.toJSON(), type: "user" }));
+    res.locals.searchResult = [...threads, ...users].sort(
+      (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
+    );
+  }else{
+    res.locals.searchResult = null;
+  }
+
+
+  res.locals.loggingInUser = await models.User.findByPk(userId, (err, user) => {
+    if (err) {
+      return res.status(500).send("Error retrieving user information");
+    }
+  });
+  res.locals.users = await models.User.findAll({
+    where: {
+      id: {
+        [Op.ne]: userId, // Exclude the logged-in user
+      },
+    },
+  });
+  const following = await models.Follower.findAll({
+    where: { follower_id: userId },
+    attributes: ['following_id'],
+  });
+
+  const followingIds = following.map(item => item.following_id);
+
+  res.locals.followingIds = followingIds; // Pass to the view
+  res.render("search", { headerName: "Search", page: 2 });
+};
+
+controller.markRead = async (req, res) => {
+  const { notiId } = req.params;
+
+  try {
+    const noti = await models.Notification.findOne({ where: { id: notiId } });
+
+    if (!noti) {
+      return res.redirect('/Activity');
+    }
+
+    noti.is_read = true;
+    await noti.save();
+ 
+    res.send("Mark as read successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+};
+
+controller.deleteNoti = async (req, res) => {
+  const { notiId } = req.params;
+
+  try {
+    await models.Notification.destroy({ where: { id: notiId } });
+
+    res.send("Delete notification successfully");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "An error occurred" });
+  }
+}
+
+controller.showActivity = async(req, res) => {
+  const userId = req.session.userId;
+  res.locals.loggingInUser = await models.User.findByPk(userId, (err, user) => {
+    if (err) {
+      return res.status(500).send('Error retrieving user information');
+    }
+  });
+
+  res.locals.notifications = await models.Notification.findAll({
+    include: [
+      {
+        model: models.User,
+        as: 'transfererFk', // Specify alias for transferer_id
+      },
+    ],
+    where: { receiver_id: userId },
+    order: [["id", "ASC"]],
+  });
+
+  res.locals.notifications.forEach(notification => {
+    notification.isFollowMessage = notification.content === "Follow you";
+  });
+  
+  
+  const following = await models.Follower.findAll({
+    where: { follower_id: userId },
+    attributes: ['following_id'],
+  });
+
+  const followingIds = following.map(item => item.following_id);
+
+  res.locals.followingIds = followingIds; // Pass to the view
+  res.render("activity", {headerName: "Activity", page: 3});
+}
+
+controller.editProfile = async (req, res) => {
+  console.log(req.body);
+  console.log("File uploaded:", req.file);
+  const {username, bio} = req.body;
+  const userId = req.session.userId;
+
+  let profile_picture = undefined;
+
+  if (req.file) {
+    profile_picture = `/img/profile/${req.file.filename}`; 
+  }
+
+  try {
+    await models.User.update(
+      {
+        ...(username !== undefined && { username }),
+        ...(profile_picture !== undefined && { profile_picture }),
+        ...(bio !== undefined && { bio }),
+      }, 
+      {
+      where: { id: userId },
+    });
+
+    res.send("User has been updated");
+  }catch(error){
+    console.error(error);
+    res.status(500).send("Cannot update user");
+  }
+}
+
+controller.showProfile = async (req, res) => {
+
+    const userId = req.session.userId;
+
+    res.locals.currentUser = await models.User.findByPk(userId, (err, user) => {
+      if (err) {
+        return res.status(500).send('Error retrieving user information');
+      }
+    });
+    res.locals.loggingInUser = res.locals.currentUser;
+
+    const following = await models.User.findAll({
+      attributes: ["id"],
+      include: [
+        {
+          model: models.Follower,
+          where: { follower_id: userId },
+          required: true,
+          attributes: [],
+        },
+      ],
+    }).then((following) => following.map((following) => following.id)); 
+
+    res.locals.following = await models.Follower.findAll({
+      where: { follower_id: userId },
+      include: [
+        {model: models.User, as: 'followingFk'},
+      ]
+    });
+
+    res.locals.follower = ( await models.Follower.findAll({
+      where: { following_id: userId },
+      include: [
+        {model: models.User, as: 'followerFk'},
+      ]
+    })
+    ).map((eachFollower) => {
+      const plainFollower = eachFollower.get({ plain: true });
+      
+      plainFollower.isFollowed = following.includes(plainFollower.followerFk.id);
+      return plainFollower;
+    });
+    
+    res.locals.threads = await models.Thread.findAll({
+      attributes: ['id', 'user_id', 'content', 'createdAt'],
+      where: {
+        user_id: userId
+      },
+      include: [
+        {
+          model: models.User,
+          required: false, // This performs an LEFT JOIN
+        },
+        {model: models.Media},
+        {model: models.Like,
+          required: false,
+          where: { user_id: userId },
+        },
+      ]
+    });
+
+    res.locals.reposts = await models.Repost.findAll({
+      where: { user_id: userId },
+      include: [
+        {
+          model: models.Thread, // Join with the Thread table
+          attributes: ["id", "content", "createdAt"], // Select specific thread fields
+          include: [ 
+            {model: models.User, attributes: ["id", "username", "profile_picture"]}, 
+            {model: models.Media},
+          ],
+        },
+      ],
+    });
+
+    const isCurrentUser = true;
+    res.locals.threads.forEach(thread => {
+      thread.isLiked = thread.Likes.length > 0;  // If the current user liked the thread, set isLiked to true
+    });
+    res.locals.isCurrentUser = isCurrentUser;
+    res.render("profile", {headerName: "Profile", page: 4});
+}
+
+controller.showIDProfile = async (req, res) => {
+  const loginId = req.session.userId;
+  const userId = req.params.id;
+  if(userId == loginId){
+    return res.redirect("/Profile");
+  }
+  res.locals.currentUser = await models.User.findByPk(userId, (err, user) => {
+    if (err) {
+      return res.status(500).send('Error retrieving user information');
+    }
+  });
+  res.locals.loggingInUser = await models.User.findByPk(loginId, (err, user) => {
+    if (err) {
+      return res.status(500).send('Error retrieving user information');
+    }
+  });
+
+  res.locals.follower = await models.Follower.findAll({
+    where: { following_id: userId },
+    include: [
+      {model: models.User, as: 'followerFk'},
+    ]
+  });
+
+  res.locals.following = await models.Follower.findAll({
+    where: { follower_id: userId },
+    include: [
+      {model: models.User, as: 'followingFk'},
+    ]
+  });
+
+  res.locals.threads = await models.Thread.findAll({
+    attributes: ['id', 'user_id', 'content', 'createdAt'],
+    where: {
+      user_id: userId
+    },
+    include: [
+      {
+        model: models.User,
+        required: false, // This performs an LEFT JOIN
+      },
+      {model: models.Media},
+      {model: models.Like,
+        required: false,
+        where: { user_id: loginId },
+      },
+    ]
+  });
+
+    res.locals.reposts = await models.Repost.findAll({
+      where: { user_id: userId },
+      include: [
+        {
+          model: models.Thread, // Join with the Thread table
+          attributes: ["id", "content", "createdAt"], // Select specific thread fields
+          include: [ 
+            {model: models.User, attributes: ["id", "username", "profile_picture"]}, 
+            {model: models.Media},      
+          ],
+        },
+      ],
+    });
+
+  // Check if the logged-in user is following the current user
+    const isFollowed = await models.Follower.findOne({
+        where: {
+            follower_id: loginId,
+            following_id: userId,
+        }
+    });
+    
+    res.locals.isFollowed = isFollowed ? true : false;  // If a follow record exists, mark as followed
+
+    res.locals.threads.forEach(thread => {
+      thread.isLiked = thread.Likes.length > 0;  // If the current user liked the thread, set isLiked to true
+    });
+    const isCurrentUser = false;
+    res.locals.isCurrentUser = isCurrentUser;
+
+    res.render("profile", {headerName: "Profile", page: 4});
+}
+
+controller.showPostDetails = async (req, res) => {
+    const postid = req.params.postid;
+    const currentUserId = req.session.userId;
+    res.locals.loggingInUser = await models.User.findByPk(currentUserId, (err, user) => {
+      if (err) {
+        return res.status(500).send('Error retrieving user information');
+      }
+    });
+
+    const thread = await models.Thread.findOne({
+      attributes: ['id', 'user_id', 'content', 'createdAt'],
+      include: [
+        {model: models.User},
+        {model: models.Media},
+        {model: models.Comment,
+          include: [{model: models.User,
+            attributes: ['username', 'profile_picture']
+          }]
+        },
+        {model: models.Like,
+          attributes: ['user_id'],
+        },
+        {model: models.Repost,
+          attributes: ['user_id'],
+        }
+      ],
+      where: {
+        id: postid
+      }
+    });
+
+    const isLiked = thread.Likes.some(like => like.user_id === currentUserId);
+    console.log("Is Liked:", isLiked);
+    thread.isLiked = isLiked;
+
+    res.locals.thread = thread;
+    res.render("post_details", {headerName: "Post", page: 5});
+}
+
+controller.showLogin = (req, res) => {
+  const errorMessage = req.flash('errorMessage');
+  req.session.destroy(err => {
+    if (err) {
+      console.error('Failed to destroy session:', err);
+    }
+  });
+
+  return res.render('login', {
+    layout: 'account',
+    title: 'Login',
+    errorMessage,   
+  });
+}
+
+controller.showCreate = (req, res) => {
+    res.render("create", {layout: "account", title: 'Sign Up'});
+}
+
+const sendVerificationEmail = async (email, verificationToken) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail', // or any email service
+    auth: {
+      user: 'ducnguyentemp@gmail.com', // replace with your email
+      pass: 'kcvs jqcq gfxr kjon',   // replace with your email password
+    },
+  });
+
+  const verificationLink = `localhost:3000/CreateAccount/VerifyEmail?token=${verificationToken}`;
+
+  const mailOptions = {
+    from: 'ducnguyentemp@gmail.com',
+    to: email,
+    subject: 'Email Verification Link',
+    html: `<p>Please click the link below to verify your email:</p><p><a href="localhost:3000/CreateAccount/VerifyEmail?token=${verificationToken}">Click here to verify your email</a></p><p>The verification link is: localhost:3000/CreateAccount/VerifyEmail?token=${verificationToken}</p>`,
+  };
+
+  await transporter.sendMail(mailOptions);
+};
+
+controller.addUser = async (req, res) => {
+  const { email, phonenumber, username, password } = req.body;
+
+  try {
+    const existingEmail = await models.User.findOne({ where: { email } });
+    if (existingEmail) {
+      return res.render("create", {
+        layout: "account",
+        title: "Sign Up",
+        errorMessage: `An account with this email: ${email} is already exists.`,
+      });
+    }
+
+    const existingPhone = await models.User.findOne({ where: { phonenumber } });
+    if (existingPhone) {
+      return res.render("create", {
+        layout: "account",
+        title: "Sign Up",
+        errorMessage: `An account with this phone number: ${phonenumber} is already exists.`,
+      });
+    }
+
+    const existingUsername = await models.User.findOne({ where: { username } });
+    if (existingUsername) {
+      return res.render("create", {
+        layout: "account",
+        title: "Sign Up",
+        errorMessage: `An account with this username: ${username} is already exists.`,
+      });
+    }
+
+    const verificationToken = crypto.randomBytes(32).toString('hex');
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await models.User.create({
+      email,
+      phonenumber,
+      username,
+      password: hashedPassword,
+      bio: "Not added yet",
+      profile_picture: "/img/profile/defaultAvatar.jpeg",
+      verificationToken,
+      isVerified: false, 
+    });
+
+    await sendVerificationEmail(email, verificationToken);
+    return res.render("create", {
+      layout: "account",
+      title: "Sign Up",
+      errorMessage: "Account created successfully. Please check your email to verify your account.",
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+controller.login = async (req, res) => {
+  const { usernameOrEmail, password } = req.body; // Get login credentials from the request body
+  try {
+    // Check if the input is an email or username
+    let user;
+    if (usernameOrEmail.includes('@')) {
+      // If it's an email (contains '@'), search by email
+    user = await models.User.findOne({
+        where: {
+          email: usernameOrEmail,
+        },
+      });
+    } 
+    else {
+      // If it's a username, search by username
+      user = await models.User.findOne({
+        where: {
+          username: usernameOrEmail,
+        },
+      });
+    }
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.render("login", {
+        layout: "account",
+        title: "Login",
+        errorMessage: "Invalid username, email or password.",
+      });
+    }
+
+    if (!user.isVerified) {
+      return res.render("login", {
+        layout: "account",
+        title: "Login",
+        errorMessage: "This account has not been verified. Please check your email.",
+      });
+    }
+
+    // Password is correct, redirect to Homepage
+    req.session.userId = user.id;
+    return res.redirect("/Homepage");
+  } catch (error) {
+    console.error(error);
+    res.status(500).render("login", {
+      layout: "account",
+      title: "Login",
+      errorMessage: "An error occurred. Please try again later."
+    });
+  }
+};
+
+module.exports = controller;
